bc15278cd0fb5c60ad41954a524d47a883cf2720
silas <silasgebhart12@gmail.com>
2024-02-06 13:31:05 -0600

    distance from HQ in Blocks

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks * 264);
+}
