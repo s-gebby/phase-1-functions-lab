5926205f96e7051e8c29e5a631ff7c86e4d85120
silas <silasgebhart12@gmail.com>
2024-02-10 17:36:34 -0600

    fixed the fare price

--- index.js
+++ index.js
@@ -4,5 +4,26 @@ function distanceFromHqInBlocks(blocks) {
 }
 
 function distanceFromHqInFeet(blocks) {
-  return distanceFromHqInBlocks(blocks * 264);
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  let farePrice;
+
+  if (distance <= 400) {
+    farePrice = 0;
+  } else if (distance > 400 && distance <= 2000) {
+    farePrice = (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    farePrice = 25;
+  } else {
+    farePrice = "cannot travel that far";
+  }
+
+  return farePrice;
 }
